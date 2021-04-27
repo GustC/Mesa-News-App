f831e9647527c04cebffc1485f13fac0d490736f
GustC <gustavogu44@hotmail.com>
2021-04-27 20:05:58 -0300

    Modificando Theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:mesa_news_app/constants/colors.dart';
 import 'package:mesa_news_app/screens/wellcome/wellcome_page.dart';
 
 class MesaNewsApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mesa News',
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: primaryColor,
+        appBarTheme: AppBarTheme(
+          color: primaryColor,
+          centerTitle: true,
+          brightness: Brightness.dark,
+          iconTheme: IconThemeData(color: Colors.white),  
+          elevation: 0,   
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontFamily: "roboto"
+          )     
+        ),
       ),
+      debugShowCheckedModeBanner: false,
       home: WellcomePage(),
     );
   }
