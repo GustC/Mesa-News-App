0e7afc8d7af9030ad7e743239312259225b9ebd2
GustC <gustavogu44@hotmail.com>
2021-04-27 20:56:24 -0300

    Tela de entrada

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const Color primaryColor = const Color(0xff010A53);
+const Color wellcomeButtonTextColor = const Color(0xff0075FF);
--- lib/screens/wellcome/wellcome_page.dart
+++ lib/screens/wellcome/wellcome_page.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mesa_news_app/constants/colors.dart';
 
 class WellcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height * 0.15;
     return Scaffold(
-        body: Center(
-      child: Text("wellcome"),
-    ));
+      backgroundColor: primaryColor,      
+      body: Center(
+        child: Column(          
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SvgPicture.asset(
+                  "assets/images/logo.svg",
+                  height: height,
+                  fit: BoxFit.fill,
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 30),
+                  child: renderTitle(),
+                ),
+              ],
+            ),            
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: (){
+
+                      },                      
+                      style: OutlinedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(vertical: 20),                    
+                      ),                      
+                      child: Text(
+                        "Entrar com e-mail",
+                        style: TextStyle(
+                          color: wellcomeButtonTextColor,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  renderTitle(){
+    var textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 25,
+      fontWeight: FontWeight.bold
+    );
+    return Wrap(
+      spacing: 25,
+      children: [
+        Text("N",style: textStyle,),
+        Text("E",style: textStyle,),
+        Text("W",style: textStyle,),
+        Text("S",style: textStyle,),
+      ],
+    );
   }
 }
